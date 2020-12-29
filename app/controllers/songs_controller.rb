87677776e561b4c2969c533fb83810403954b992
James Donovan <jameswilliamdonovan@gmail.com>
2020-12-29 06:31:55 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -7,4 +7,15 @@ class SongsController < ApplicationController
   get "/songs/new" do
     erb :"/songs/new"
   end
+
+  get '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+
+    erb :'songs/show'
+  end
+
+  get "/songs/:slug" do
+    @song = Song.find_by_slug(params[:slug])
+    erb :"songs/show"
+  end
 end
