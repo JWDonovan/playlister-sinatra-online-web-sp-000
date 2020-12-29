33dca8c82c6ed5ad70ece89fbe8ea19e43b43f67
James Donovan <jameswilliamdonovan@gmail.com>
2020-12-29 06:33:19 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -28,4 +28,16 @@ class SongsController < ApplicationController
     @song = Song.find_by_slug(params[:slug])
     erb :"songs/edit"
   end
+
+  patch "/songs/:slug" do
+    @song = Song.find_by_slug(params[:slug])
+
+    @song.update(params[:song])
+    @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
+    @song.genre_ids = params[:genres]
+    @song.save
+
+    flash[:message] = "Successfully updated song."
+    redirect("/songs/#{@song.slug}")
+  end
 end
